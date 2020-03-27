0b5d5175f03c1a53b6d2778a8ff8d932535fdca5
Tulio Camargo de Azevedo <tuliocaz.windcoast@gmail.com>
2020-03-27 18:33:00 -0300

    feat: first unit test created

--- src/controller/OngController.js
+++ src/controller/OngController.js
@@ -1,4 +1,4 @@
-import crypto from 'crypto';
+import generateUniqueId from '../utils/generateUniqueId';
 
 import connection from '../database/connection';
 
@@ -11,7 +11,7 @@ export default {
 
   async store(req, res) {
     const { name, email, whatsapp, city, uf } = req.body;
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = generateUniqueId();
 
     await connection('ongs').insert({
       id,
