46268828b8493f5085204da764a4fb1a395202e5
Tulio Camargo de Azevedo <tuliocaz.windcoast@gmail.com>
2020-03-27 18:31:27 -0300

    config: Configuring jest for testing tool

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,13 @@ module.exports = {
   env: {
     es6: true,
     node: true,
+    "jest/globals": true
   },
   extends: [
     'airbnb-base',
     'prettier',
   ],
-  plugins: [ 'prettier' ],
+  plugins: [ 'prettier', 'jest' ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -22,5 +23,10 @@ module.exports = {
     "camelcase": "off",
     "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
     "func-names": "off",
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error"
   },
 };
--- src/server.js
+++ src/server.js
@@ -1,16 +1,3 @@
-import 'dotenv/config';
-
-import express from 'express';
-import cors from 'cors';
-import { errors } from 'celebrate';
-
-import routes from './routes';
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(routes);
-app.use(errors());
+import app from './app';
 
 app.listen(3333);
