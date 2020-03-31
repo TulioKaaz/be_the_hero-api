94597dd13cd8155858fcd5a5b78ffc0cbe89cdd3
Tulio Camargo de Azevedo <tuliocaz.windcoast@gmail.com>
2020-03-30 22:06:19 -0300

    refactor: CreateOngs validator refactoring

--- src/validator/ongsValidator.js
+++ src/validator/ongsValidator.js
@@ -4,7 +4,11 @@ export default {
   cereateOng: celebrate({
     [Segments.BODY]: Joi.object().keys({
       email: Joi.string().required().email(),
-      password: Joi.string().required().min(4),
+      password: Joi.string().min(4).required().strict(),
+      confirmPassword: Joi.string()
+        .valid(Joi.ref('password'))
+        .required()
+        .strict(),
       name: Joi.string().required(),
       whatsapp: Joi.string().min(10).max(11),
       city: Joi.string().required(),
